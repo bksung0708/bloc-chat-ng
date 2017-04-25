b82fc8285030eba16c5faf7f6b29457c8441a411
Brian Sung <bksung0708@gmail.com>
2017-04-24 23:35:10 -0700

    assignment 2 completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-    function HomeCtrl($scope, Room) {
-        $scope.chatRooms = Room.all;
+    function HomeCtrl(Room) {
+        this.chatRooms = Room.all;
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
